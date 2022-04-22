e1d199e90d29f1844dd25110b68a78c5c1ddd531
wldud01 <64887559+wldud01@users.noreply.github.com>
2022-04-22 19:33:39 +0900

    Update server.js

--- prev_book_page/server/server.js
+++ prev_book_page/server/server.js
@@ -1,4 +1,3 @@
-<<<<< HEAD
 const express = require("express");
 const app = express();
 const api = require("./routes/index");
@@ -10,20 +9,4 @@ app.use("/api", api);
 
 app.listen(3001, () =>
   console.log("Node.js Server is ruuning on port 3001...")
-=======
-const express = require('express');
-const app = express();
-const api = require('./routes/index');
-const PORT = 3001;
-
-const cors = require('cors');
-app.use(cors());
-
-app.use(express.static('shop_images'));
-
-app.use('/api', api);
-
-app.listen(PORT, () =>
-  console.log('Node.js Server is ruuning on port 3001...'),
->>>>>>> e5824205ad86236c9dea334db8d89bb3486a02b2
 );
